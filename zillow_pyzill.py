fbaca0fb6cb7f926dc380f22f464c1751b89a5e5
Davina <davinam717@gmail.com>
2025-01-06 20:08:52 -0800

    remove print statements

--- zillow_pyzill.py
+++ zillow_pyzill.py
@@ -33,19 +33,15 @@ results_sale = pyzill.for_sale(
     ne_lat=ne_lat, ne_long=ne_long, sw_lat=sw_lat, sw_long=sw_long,
     zoom_value=10,
 )
-for k, v in results_sale.items():
-    if isinstance(v, list):
-        print(k, len(v))
-    else:
-        print(k, v)
+
 map_results = results_sale['mapResults']
 print(f"found {len(map_results)} results")
 
-with open("./jsondata_sale.json", "w") as f:    
-    f.write(json.dumps(results_sale, indent=4))
+# with open("./jsondata_sale.json", "w") as f:    
+#     f.write(json.dumps(results_sale, indent=4))
 
-with open("./map_results.json", "w") as f:
-    f.write(json.dumps(map_results, indent=4))
+# with open("./map_results.json", "w") as f:
+#     f.write(json.dumps(map_results, indent=4))
 
 fieldnames = [
     "type",
@@ -63,7 +59,6 @@ fieldnames = [
     "isFavorite",
     "listingType",
 ]
-print(fieldnames)
 
 cols_to_consolidate = [
     "area",
@@ -84,23 +79,8 @@ def get_zipcode(address):
 df = pd.DataFrame(map_results)
 df["zipcode"] = df["address"].apply(get_zipcode)
 df["timeOnZillow"] = (df.timeOnZillow / 1000 / 60 / 60 / 24).astype(int)
-print(df.columns)
 for col in cols_to_consolidate:
     df[col].fillna(df[f"min{col.title()}"], inplace=True)
 df = df.rename(columns=cols_to_rename)[fieldnames]
 
-print(df.shape)
-print(df.head())
-print(df.iloc[0,])
-print(df.dtypes)
-
 df.to_csv("./map_results.csv")
-
-
-# with open("./map_results.csv", "w") as f:
-#     writer = csv.DictWriter(
-#         f, 
-#         fieldnames=fieldnames
-#     )
-#     writer.writeheader()
-#     writer.writerows(map_results)
